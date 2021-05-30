53ee1c9dad267728e18fe60dcc438bd0498808c2
bodya <vishnyaa3d@gmail.com>
2021-05-30 19:00:26 +0300

    Commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");     // для кросдоменных запросов
 const cookieParser = require("cookie-parser");
-const parser = require("./middleware/parse")
+// const parser = require("./middleware/parse")
 const app = express();
 const PORT = 4123;
 app.use(cors());    //Кроссдоменные запросы
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("public"));  //для работы с формами
 app.use(cookieParser());            //для работы с куки
-app.use(require("./routes/router.js",parser.job.start()));
+app.use(require("./routes/router.js"));
 app.listen(PORT, () => {
     console.log(`https://obscure-taiga-00490.herokuapp.com:${PORT}`);
 });
