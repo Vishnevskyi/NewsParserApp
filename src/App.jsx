e3c27e5231c56505a1cea00aa9888efc66c74e9c
Vishnevskyi <76432370+Vishnevskyi@users.noreply.github.com>
2021-05-30 13:34:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import aboutPage from "./about/about"
 function App() {
     const [article, setArticle] = useState([]);
     useEffect(() => {
-        fetch("https://murmuring-forest-06470.herokuapp.com/api:37954/article", {
+        fetch("https://murmuring-forest-06470.herokuapp.com/api:4000/article", {
             method: "POST",
             mode: "cors"
         })
@@ -20,7 +20,7 @@ function App() {
     }, [])
     const [news, setNews] = useState([]);
     useEffect(() => {
-        fetch("https://murmuring-forest-06470.herokuapp.com:37954/api/news", {
+        fetch("https://murmuring-forest-06470.herokuapp.com:4000/api/news", {
             method: "POST",
             mode: "cors"
         })
