bd21d10428f131cdf835156817aab197ed9d7bc5
Vishnevskyi <76432370+Vishnevskyi@users.noreply.github.com>
2021-05-30 11:41:30 +0300

    Update task.jsx

--- src/admin/task/task.jsx
+++ src/admin/task/task.jsx
@@ -3,7 +3,7 @@ const { useState, useEffect } = require("react")
 const Task = () =>{
     const [task,setTask] = useState([]);
     useEffect(()=>{
-        fetch("http://localhost:4000/api/aside",{
+        fetch("damp-tundra-84467.herokuapp.com/aside",{
             method: "POST",
             mode: "cors"
         }).then(res => res.json())
